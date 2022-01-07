099ec5fc0abbbd7082369899a6ad5a9d82a05568
JOHN ALVARO RUEDA FORERO <risgan2727@gmail.com>
2022-01-07 00:00:39 -0500

    Cordova

--- src/app/page/paciente/paciente-routing.module.ts
+++ src/app/page/paciente/paciente-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemperaturaComponent } from 'src/app/components/paciente/temperatura/temperatura.component';
 
 import { PacientePage } from './paciente.page';
 
 const routes: Routes = [
-  {path: '',component: PacientePage }
+  {path: '',component: PacientePage },
+  // {path: 'temperatura',loadChildren: () => import('./../../components/paciente/temperatura/temperatura.component').then( m => m.TemperaturaComponent) },
+  {path: 'temperatura',component: TemperaturaComponent },
+
 ];
 
 @NgModule({
--- src/app/page/paciente/paciente.page.ts
+++ src/app/page/paciente/paciente.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 
 @Component({
@@ -11,7 +12,8 @@ export class PacientePage implements OnInit {
   nombreUsuario: string;
 
   constructor(
-    private menu: MenuController
+    private menu: MenuController,
+    private router: Router,
   ) { }
 
   ngOnInit() {
@@ -31,5 +33,22 @@ export class PacientePage implements OnInit {
     this.menu.enable(true, 'custom');
     this.menu.open('custom');
   }
+
+  temperaturaIr(){
+    console.log("hola")
+    this.router.navigate(['temperatura'])
+  }
+  // loguear(){
+  //   if(this.formLogin.value.usuario=="Hola"){
+  //     this.isLogin=true;
+  //     setTimeout(() => {
+  //       this.isLogin=false;
+  //       this.router.navigate(['paciente'])
+  //     }, 3000);
+  //   }else{
+  //     this.alertaNoLogin();
+  //     this.formLogin.reset();
+  //   }
+  // }
 
 }
